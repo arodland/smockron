5ba6a7771e26d68a0315f3dfb4223508e11314b7
Andrew Rodland <andrew@cleverdomain.org>
2014-02-25 14:59:37 -0500

    Add shebangs for scripts

--- bin/gatekeeper_test.js
+++ bin/gatekeeper_test.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var express = require('express'),
     Smockron = require('../lib/smockron');
 
--- bin/master.js
+++ bin/master.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var Smockron = require('../lib/smockron'),
     config = require('config');
 
